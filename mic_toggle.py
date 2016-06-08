12ccb82e8c328a823ce3d6577ecfd8c097b6a563
Simone Guercio <s.guercio@gmail.com>
2016-06-08 15:06:06 +0200

    New exit condition, subprocess cleanup
    The old exit condition was garbage on any system but mine,
    so I deleted it and tried to make something more portable.
    I might have understood how to make proper use of subprocess, too.

--- mic_toggle.py
+++ mic_toggle.py
@@ -8,55 +8,37 @@ import sys
 
 #Define a function called at every keypress
 def kbevent_down(event):
+    global nome_terminale
     if event.Ascii == tasto_ascii:
-        subprocess.call('amixer -q set Capture cap', shell=True)
-    elif event.Ascii == 113 and event.WindowName[-7:] == 'toggler':
+        subprocess.call(['amixer', '-q', 'set', 'Capture', 'cap'])
+    elif event.Ascii == 113 and event.WindowName == nome_terminale:
         global running
         running = False
 
-        
+
 def kbevent_up(event):
     if event.Ascii == tasto_ascii:
-        subprocess.call('amixer -q set Capture nocap', shell=True)
+        subprocess.call(['amixer', '-q', 'set', 'Capture', 'nocap'])
 
 
-#Pre flight checks
-lista_output = ['amixer get Capture | grep "\[on\]" | head -1 | cut -d " " -f 7',
-                'amixer get Capture | grep "\[on\]" | head -1 | cut -d " " -f 8',
-                'amixer get Capture | grep "\[on\]" | head -1 | cut -d " " -f 9']
-
+id_linea = subprocess.check_output(['xprop', '-root', '_NET_ACTIVE_WINDOW']).strip()
+id_parti = id_linea.split()
+id = id_parti[-1]
+nome_linea = subprocess.check_output(['xprop', '-id', id, 'WM_NAME']).strip()
+nome_parti = nome_linea.split()
+nome_parti_utili = nome_parti[2:]
+nome_parti_utili_unite = ' '.join(nome_parti_utili)
+nome_terminale = nome_parti_utili_unite[1:-1]
 #To make sure the Capture interface is up
-subprocess.call('amixer -q set Capture cap', shell=True)
-
-#Select right amixer command
-avanzamento = 0
-for papabile in lista_output:
-    output = subprocess.check_output(papabile, shell=True).strip().decode('ascii')
-    print(str(avanzamento) + ' > ' + output)
-    avanzamento += 1
-
-verifica_stato = ''
-
-while verifica_stato == '':
-    output_selezionato = raw_input('Seleziona il numero di riga dove e\' comparso \"[on]\"\n')
-    if output_selezionato == '0':
-        verifica_stato = lista_output[0]
-    elif output_selezionato == '1':
-        verifica_stato = lista_output[1]
-    elif output_selezionato == '2':
-        verifica_stato = lista_output[2]
-    else:
-        print('selezione non valida, riprova')
-
+subprocess.call(['amixer', '-q', 'set', 'Capture', 'cap'])
 #Set the push-to-talk button
-tasto = raw_input('Ora seleziona il tasto per spegnere/accendere il microfono (NO Q, ctrl, alt, shift, cose strane)\n')
+tasto = raw_input('Seleziona il tasto per spegnere/accendere il microfono (NO Q, ctrl, alt, shift, cose strane)\n')
 tasto_ascii = ord(tasto)
-
 print('Settaggio completato, lo script e\' ora in funzione\n'
       'Il microfono e\' disattivato, tenere premuto \"' + tasto.upper() + '\" per attivarlo\n'
       'Per disattivare lo scrip premere \"Q\" in questa finestra. Fly safe!')
-subprocess.call('amixer -q set Capture nocap', shell=True)
-
+subprocess.call(['amixer', '-q', 'set', 'Capture', 'nocap'])
+subprocess.call(['notify-send', '-t', '1000', 'mic_toggle e\' ora in funzione'])
 #Create hookmanager
 hookman = pyxhook.HookManager()
 #Define our callback to fire when a key is pressed down/up
@@ -66,15 +48,13 @@ hookman.KeyUp = kbevent_up
 hookman.HookKeyboard()
 #Start our listener
 hookman.start()
-    
 #Let's start this thing
 running = True
 while running:
     time.sleep(0.1)
-
 #Put the Capture interface back up
-subprocess.call('amixer -q set Capture cap', shell=True)
-print('Grazie per aver utilizzato python_mic_toggle!')
-
+subprocess.call(['amixer', '-q', 'set', 'Capture', 'cap'])
+print('\nScript terminato. Grazie per aver utilizzato python_mic_toggle!')
+subprocess.call(['notify-send', '-t', '1000', 'mic_toggle e\' stato disattivato'])
 #Close the listener when we are done
 hookman.cancel()
